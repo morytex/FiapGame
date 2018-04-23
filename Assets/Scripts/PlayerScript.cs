05f7983da6cf0326612057d4fd8f28fbae2f4d19
Alessandro <alemoryta@gmail.com>
2018-04-22 21:01:48 -0300

    Combining run and jump animations with movements

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
 	public float velocidade;
-//	public float impulso;
+	public float impulso;
 
-//	public Transform chaoVerificador;
-//	bool estaoNoChao;
+	public Transform chaoVerificador;
+	bool estaoNoChao;
 
-//	SpriteRenderer spriteRender;
-//	Rigidbody2D rb;
-//	Animator animator;
+	SpriteRenderer spriteRender;
+	Rigidbody2D rb;
+	Animator animator;
 
 	void Start () {
 		// Interface para os componentes
-//		spriteRender = GetComponent<SpriteRenderer>();
-//		rb = GetComponent<Rigidbody2D> ();
-//		animator = GetComponent<Animator>();
+		spriteRender = GetComponent<SpriteRenderer>();
+		rb = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator>();
 	}
 
 	void Update () {
@@ -27,29 +27,34 @@ public class PlayerScript : MonoBehaviour {
 		float mover_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
 		transform.Translate (mover_x, 0.0f, 0.0f);
 
-//		// Verificar colisao com o piso
-//		estaoNoChao = Physics2D.Linecast(transform.position, 
-//			chaoVerificador.position, 1 << LayerMask.NameToLayer("Piso"));
+		// Verificar colisao com o piso
+		estaoNoChao = Physics2D.Linecast(transform.position, 
+			chaoVerificador.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		print ("transform.position");
+		print (transform.position);
+
+		print ("chaoVerificador.position");
+		print (chaoVerificador.position);
+
+		print ("LayerMask.NameToLayer(\"Ground\")");
+		print (LayerMask.NameToLayer("Ground"));
+
+		// Pulo
+		if (Input.GetButtonDown ("Jump") && estaoNoChao) {
+			rb.velocity = new Vector2 (0.0f, impulso); 
+		}
+
+		// Orientacao
+		if (mover_x > 0) {
+			spriteRender.flipX = false; 
+		} else if (mover_x < 0) {
+			spriteRender.flipX = true; 
+		}
 //
-//
-//		print (estaoNoChao);
-//
-//
-//		// Pulo
-//		if (Input.GetButtonDown ("Jump") && estaoNoChao) {
-//			rb.velocity = new Vector2 (0.0f, impulso); 
-//		}
-//
-//		// Orientacao
-//		if (mover_x > 0) {
-//			spriteRender.flipX = false; 
-//		} else if (mover_x < 0) {
-//			spriteRender.flipX = true; 
-//		}
-//
-//		// Animacoes
-//		animator.SetFloat("pMover", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
-//		animator.SetBool("pJump", !estaoNoChao); 
+		// Animacoes
+		animator.SetFloat("pMove", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+		animator.SetBool("pJump", !estaoNoChao); 
 //		animator.SetBool("pFire", Input.GetButton("Fire1")); 
 
 	}
