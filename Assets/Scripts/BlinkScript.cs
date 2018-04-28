596992f91045e623a033a8d9e4efec1c2c2d61f5
Alessandro <alemoryta@gmail.com>
2018-04-28 12:28:12 -0300

    Changing game theme and applying introduction scene

---
+++ Assets/Scripts/BlinkScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BlinkScript : MonoBehaviour {
+
+	public float intervalo;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		GetComponent<SpriteRenderer> ().enabled = 
+			!GetComponent<SpriteRenderer> ().enabled;
+		yield return new WaitForSeconds(intervalo);
+		StartCoroutine (Start ());
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			SceneManager.LoadScene ("main");
+		}
+	}
+}
